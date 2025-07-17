abd4d54cc8faf15f59b6c59f0ab831936d1f82da
Gaurav <gauravkrdas19@gmail.com>
2025-07-18 00:27:45 +0530

    navbar logo done

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between bg-xanthous px-4 lg:px-26 py-2'>
+            <div className='flex gap-2 lg:gap-4'>
+                <img
+                    src="/Breakfast Streamline Lagos.png"
+                    className='w-12 md:w-30'
+                    draggable='false'
+                />
+                <div className="flex flex-col justify-center">
+                    <h1 className='text-night font-italiana text-4xl lg:text-7xl'>PanPal</h1>
+                    <p className='text-night font-juliusSansOne text-[10px] lg:text-xl uppercase leading-1 lg:leading-4 mb-1'>your personalized recipe-book</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
